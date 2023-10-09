b80564b47777893b00f25ec4d148e773c38ac403
kelvin1095 <k.wong1095@gmail.com>
2023-10-09 13:06:11 +1100

    9Oct Added Tracing

--- src/main.rs
+++ src/main.rs
@@ -3,90 +3,91 @@ use axum::{
     extract::MatchedPath,
     http::{HeaderMap, Request},
     response::Response,
-    routing::get,
-    Router,
+    Router
 };
 use tokio::signal;
 use tower_http::{
     classify::ServerErrorsFailureClass,
-    services::{ServeDir, ServeFile},
-    trace::TraceLayer,
+    services::{ServeDir, ServeFile}, 
+    trace::TraceLayer
 };
 use tracing::{info_span, Span};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 use std::net::SocketAddr;
 use std::time::Duration;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-                // axum logs rejections from built-in extractors with the `axum::rejection`
-                // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
-                "example_tracing_aka_logging=debug,tower_http=debug,axum::rejection=trace".into()
-            }),
-        )
-        .with(tracing_subscriber::fmt::layer())
+    tracing_subscriber::fmt()
         .init();
 
     // Specify the IP address and port to listen on
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    let serve_dir = ServeDir::new("public")
-        .not_found_service(ServeFile::new("public/index.html"));
-
-    let app = Router::new()
-        .route("/foo", get(|| async { "Hi from /foo" }))
-        .layer(
-            TraceLayer::new_for_http()
-                .make_span_with(|request: &Request<_>| {
-                    // Log the matched route's path (with placeholders not filled in).
-                    // Use request.uri() or OriginalUri if you want the real path.
-                    let matched_path = request
-                        .extensions()
-                        .get::<MatchedPath>()
-                        .map(MatchedPath::as_str);
-
-                    info_span!(
-                        "http_request",
-                        method = ?request.method(),
-                        matched_path,
-                        some_other_field = tracing::field::Empty,
-                    )
-                })
-                .on_request(|_request: &Request<_>, _span: &Span| {
-                    // You can use `_span.record("some_other_field", value)` in one of these
-                    // closures to attach a value to the initially empty field in the info_span
-                    // created above.
-                })
-                .on_response(|_response: &Response, _latency: Duration, _span: &Span| {
-                    // ...
-                })
-                .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {
-                    // ...
-                })
-                .on_eos(
-                    |_trailers: Option<&HeaderMap>, _stream_duration: Duration, _span: &Span| {
-                        // ...
-                    },
-                )
-                .on_failure(
-                    |_error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
-                        // ...
-                    },
-                ),
-        )
-        .nest_service("/public", serve_dir.clone())
-        .fallback_service(serve_dir);
-
+    // Create an Axum application
+    let mut app = Router::new();
+    app = add_static_routes(app);
+    app = add_tracing(app);
+            
     // Start the Axum server
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .with_graceful_shutdown(shutdown_signal())
         .await
         .expect("Server failed to start");
+}
+
+fn add_static_routes(router: Router) -> Router {
+    let serve_dir = ServeDir::new("public")
+        .not_found_service(ServeFile::new("public/index.html"));
+
+    return router
+        .nest_service("/", serve_dir);
+}
+
+fn add_tracing(router: Router) -> Router {
+    return router
+        .layer(
+            TraceLayer::new_for_http()
+            .make_span_with(|request: &Request<_>| {
+
+                let matched_path = request
+                    .extensions()
+                    .get::<MatchedPath>()
+                    .map(MatchedPath::as_str);
+
+                info_span!(
+                    "http_request: ",
+                    // method = ?request.method(),
+                    uri = ?request.uri().path(),
+                    user_agent = ?request.headers().get("User-Agent"),
+                    matched_path,
+                    some_other_field = tracing::field::Empty,
+                )
+
+            })
+            .on_request(|request: &Request<_>, _span: &Span| {
+
+                tracing::info!("request: {} {}", request.method(), request.uri().path())
+
+            })
+            .on_response(|response: &Response, latency: Duration, _span: &Span| {
+
+                tracing::info!("response: {} in {:?}", response.status(), latency)
+
+            })
+            .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {
+                // ...
+            })
+            .on_eos(|_trailers: Option<&HeaderMap>, _stream_duration: Duration, _span: &Span| {
+                // ...
+            })
+            .on_failure(|error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
+
+                tracing::error!("error: {}", error)
+
+            })
+        )
 }
 
 async fn shutdown_signal() {
